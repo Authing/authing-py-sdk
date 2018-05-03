f5918047692b07e5da28291ec206388d359ad90e
leinue <597055914@qq.com>
2018-05-03 15:57:57 +0800

    增加accessToken初始验证

--- authing.py
+++ authing.py
@@ -1,5 +1,74 @@
 from sgqlc.endpoint.http import HTTPEndpoint
 import json
+import urllib
+
+class AuthingEndPoint(HTTPEndpoint):
+
+    def __init__(self, url, base_headers=None, timeout=None, urlopen=None):
+        HTTPEndpoint.__init__(self, url=url, base_headers=base_headers, timeout=timeout, urlopen=urlopen)
+
+    def __call__(self, query, variables=None, operation_name=None,
+                 extra_headers=None, timeout=None):
+        '''Calls the GraphQL endpoint.
+        :param query: the GraphQL query or mutation to execute. Note
+          that this is converted using ``bytes()``, thus one may pass
+          an object implementing ``__bytes__()`` method to return the
+          query, eventually in more compact form (no indentation, etc).
+        :type query: :class:`str` or :class:`bytes`.
+        :param variables: variables (dict) to use with
+          ``query``. This is only useful if the query or
+          mutation contains ``$variableName``.
+        :type variables: dict
+        :param operation_name: if more than one operation is listed in
+          ``query``, then it should specify the one to be executed.
+        :type operation_name: str
+        :param extra_headers: dict with extra HTTP headers to use.
+        :type extra_headers: dict
+        :param timeout: overrides the default timeout.
+        :type timeout: float
+        :return: dict with optional fields ``data`` containing the GraphQL
+          returned data as nested dict and ``errors`` with an array of
+          errors. Note that both ``data`` and ``errors`` may be returned!
+        :rtype: dict
+        '''
+        if isinstance(query, bytes):
+            query = query.decode('utf-8')
+        elif not isinstance(query, str):
+            # allows sgqlc.operation.Operation to be passed
+            # and generate compact representation of the queries
+            query = bytes(query).decode('utf-8')
+
+        post_data = json.dumps({
+            'query': query,
+            'variables': variables,
+            'operationName': operation_name,
+        }).encode('utf-8')
+        headers = self.base_headers.copy()
+        if extra_headers:
+            headers.update(extra_headers)
+        headers.update({
+            'Accept': 'application/json; charset=utf-8',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Content-Length': len(post_data),
+        })
+
+        self.logger.debug('Query:\n%s', query)
+
+        req = urllib.request.Request(
+            url=self.url, data=post_data, headers=headers)
+        try:
+            with urllib.request.urlopen(req, timeout=timeout) as f:
+                body = f.read().decode('utf-8')
+                try:
+                    data = json.loads(body)
+                    if data and data.get('errors'):
+                        data['errors'][0]['message']['errors'] = True
+                        return data['errors'][0]['message']
+                    return data
+                except json.JSONDecodeError as exc:
+                    return self._log_json_error(body, exc)
+        except urllib.error.HTTPError as exc:
+            return self._log_http_error(query, req, exc)        
 
 class Authing():
 
@@ -14,26 +83,44 @@ class Authing():
             "users": 'https://users.authing.cn/graphql'
         }
 
+        self.auth()
+
     def auth(self):
         if 'authService' not in self.__dict__:
-            self.authService = self.initService(self.servies['users'])
+            self.authService = self._initService(self.servies['users'])
 
-        
+            authQuery = '''
+                query getAccessTokenByAppSecret($secret: String!, $clientId: String!){
+                    getAccessTokenByAppSecret(secret: $secret, clientId: $clientId)
+                }
+            '''
+            variables = { 'clientId': self.clientId, 'secret': self.secret }
 
-        pass
+            authResult = ''
+
+            authResult = self.authService(authQuery, variables)                
+
+            if authResult.get('errors'):
+                raise Exception(authResult)
+            else:
+                self.accessToken = authResult['data']['getAccessTokenByAppSecret']
+
+                self.oauth = self._initOAuth(headers={
+                    'Authorization': 'Bearer {}'.format(self.accessToken)
+                });
+
+                self.users = self._initUsers();
 
     def _initService(self, url, headers={}):
-        # headers = {'Authorization': 'bearer TOKEN'}
-        variables = {'clientId': '59f86b4832eb28071bdd9214'}
-        endpoint = HTTPEndpoint(url, headers)
-        return endpoint
+        endpoint = AuthingEndPoint(url, headers)
+        return endpoint            
 
     def _initOAuth(self, headers={}):
-        self.oauth = self.initService(self.servies['oauth'], headers=headers)
+        self.oauth = self._initService(self.servies['oauth'], headers=headers)
         return self.oauth
 
     def _initUsers(self, headers={}):
-        self.users = self.initService(self.servies['users'], headers=headers)
+        self.users = self._initService(self.servies['users'], headers=headers)
         return self.users
 
     def login(self, email, password):
--- test.py
+++ test.py
@@ -2,13 +2,12 @@ from authing import Authing
 
 if __name__ == '__main__':
 
-    clientId = '' 
-    secret = ''
+    clientId = '5aeab91410ecb800018db165' 
+    secret = '4822165c545486ffd32c2b9d62b11936'
 
     authing = Authing(clientId, secret)
 
-    def test_auth():
-        result = authing.auth()
-        print(result)
+    oauthList = authing.readOauthList()
+    print(oauthList)
 
-    test_auth()
+    # test_auth()
