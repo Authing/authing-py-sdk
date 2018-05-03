54bfad72ece902f95ce502ca8c89a4fc443401c1
leinue <597055914@qq.com>
2018-05-03 16:20:33 +0800

    增加public key，修改注册参数传入方式

--- authing.py
+++ authing.py
@@ -127,6 +127,13 @@ class Authing():
             "users": 'https://users.authing.cn/graphql'
         }
 
+        self.pubKey = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4xKeUgQ+Aoz7TLfAfs9+paePb
+5KIofVthEopwrXFkp8OCeocaTHt9ICjTT2QeJh6cZaDaArfZ873GPUn00eOIZ7Ae
++TiA2BKHbCvloW3w5Lnqm70iSsUi5Fmu9/2+68GZRH9L7Mlh8cFksCicW2Y2W2uM
+GKl64GDcIq3au+aqJQIDAQAB
+-----END PUBLIC KEY-----"""
+
         self.auth()
 
     def auth(self):
@@ -224,11 +231,9 @@ class Authing():
         """
 
         variables = {
-            'userInfo': {
-                'email': email,
-                'password': password,
-                'registerInClient': self.clientId
-            }
+            'email': email,
+            'password': password,
+            'registerInClient': self.clientId
         }
 
         return self.users(registerQuery, variables)
