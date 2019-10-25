5c62514be5e50b2719173826c6800c3bdcfa0587
廖长江 <changjiangthecoder@gmail.com>
2019-10-25 17:52:56 +0800

    update README

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-from setuptools import setup, find_packages  
-  
-setup(  
-      name='authing',   #名称  
-      version='0.13.0',  #版本  
-      description="Authing SDK for Python", #描述  
-      keywords='authing sso AaaS IdaaS',  
-      author='authing',  #作者  
-      author_email='xieyang@authing.cn', #作者邮箱  
-      maintainer='authing',
-      maintainer_email='xieyang@authing.cn',
-      url='https://github.com/Authing/authing-py-sdk', #作者链接  
-      packages=find_packages(),  
-      include_package_data=True,  
-      zip_safe=False,  
-      install_requires=[      #需求的第三方模块  
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='authing',  # 名称
+    version='0.14.0',  # 版本
+    description="Authing SDK for Python",  # 描述
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='authing sso AaaS IdaaS',
+    author='authing',  # 作者
+    author_email='xieyang@authing.cn',  # 作者邮箱
+    maintainer='authing',
+    maintainer_email='xieyang@authing.cn',
+    url='https://github.com/Authing/authing-py-sdk',  # 作者链接
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[  # 需求的第三方模块
         'sgqlc',
         'rsa'
-      ], 
-)  
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
