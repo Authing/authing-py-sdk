631862f9f8e63f2ddad31f3b865514f562446b43
Changjiang Liao <cj@authing.cn>
2021-07-01 17:55:01 +0800

    fix: __build_oidc_authorize_url scope

--- authing/v2/authentication/authing.py
+++ authing/v2/authentication/authing.py
@@ -1211,6 +1211,7 @@ class AuthenticationClient(object):
             res['state'] = state
 
         if scope:
+            res['scope'] = scope
             if 'offline_access' in scope:
                 res['prompt'] = 'consent'
 
