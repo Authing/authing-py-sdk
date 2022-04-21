e33e4c23389a0a36045aa4da8c0968794a552f74
Changjiang Liao <cj@authing.cn>
2022-04-21 11:16:50 +0800

    chore: update setup.py install_requires

--- setup.py
+++ setup.py
@@ -44,12 +44,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     install_requires=[
-        'gql==2.0.0',
-        'graphql-core>=2.3.2,<3',
         'requests',
-        'python-dateutil',
-        'rsa==4.0'
     ]
 )
